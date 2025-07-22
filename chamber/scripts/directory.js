0c5fd15c0791423409d31cf77a3a115375c80634
AlexM2111V <jesusmendozav2111@gmail.com>
2025-07-21 23:49:23 -0500

    Add Chaber Directory Page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,67 @@
+const hambutton = document.querySelector(".menu");
+const mainnav = document.querySelector('#nav-bar');
+const header = document.querySelector('header');
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+    header.classList.toggle('show');
+})
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+
+async function loadCompanies() {
+    const response = await fetch("data/members.json");
+    const companies = await response.json();
+
+    displayCompanies(companies);
+}
+
+const displayCompanies = (companies) => {
+    const container = document.querySelector(".grid");
+    container.innerHTML = "";
+    companies.forEach(company => {
+        const card = document.createElement("section");
+        card.classList.add("company-card");
+
+        card.innerHTML = `
+      <img src="images/${company.image}" alt="Logo of ${company.name}" loading="lazy">
+      <h3>${company.name}</h3>
+      <p><strong>Address:</strong> ${company.address}</p>
+      <p><strong>Phone:</strong> ${company.phone}</p>
+      <p><strong>Website:</strong> <a href="${company.website}" target="_blank">${company.website}</a></p>
+      <p><strong>Level:</strong> ${getMembershipName(company.membershipLevel)}</p>
+    `;
+
+        container.appendChild(card);
+    });
+}
+
+function getMembershipName(level) {
+    switch (level) {
+        case 3: return "Gold";
+        case 2: return "Silver";
+        default: return "Member";
+    }
+}
+
+loadCompanies();
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
+
